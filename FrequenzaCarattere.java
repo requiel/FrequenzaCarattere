0f1674a63c50716f96603eeb0a4f743cdec38d38
requiel <xlukax_95@hotmail.it>
2014-11-08 14:25:08 +0100

    Update FrequenzaCarattere.java

--- FrequenzaCarattere.java
+++ FrequenzaCarattere.java
@@ -11,36 +11,36 @@ public class FrequenzaCarattere
 		BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
 		String tel = null;
 		boolean errore = false;
-        System.out.println("Inserisci numero di telefono: ");
-        try
-        {
+    System.out.println("Inserisci numero di telefono: ");
+    try
+    {
 			tel = tastiera.readLine();
 		}
-        catch (IOException e) {}
-        for(int i = 0; i < tel.length(); i++)
-        {
-        	char c = tel.charAt(i);
-        	if(c<'0' || c>'9')
-        	{
-        		errore = true;
-        		break;
-        	}
-        	else
-        	{
-        		int numero = Character.getNumericValue(c);
-        		frequenza[numero]++;
-        	}
-        }
-        if(errore)
-        {
-        	System.out.println("Errore non hai inserito un numero di telefono valido");
-        }
-        else
-        {
-	        for(int i = 0; i < frequenza.length; i++)
-	        {
-	        	System.out.println("numero di "+i+" : "+frequenza[i]);
-	        }
-        }
+    catch (IOException e) {}
+    for(int i = 0; i < tel.length(); i++)
+    {
+    	char c = tel.charAt(i);
+    	if(c<'0' || c>'9')
+    	{
+    		errore = true;
+    		break;
+    	}
+    	else
+    	{
+    		int numero = Character.getNumericValue(c);
+    		frequenza[numero]++;
+    	}
+    }
+    if(errore)
+    {
+    	System.out.println("Errore non hai inserito un numero di telefono valido");
+    }
+    else
+    {
+      for(int i = 0; i < frequenza.length; i++)
+      {
+      	System.out.println("numero di "+i+" : "+frequenza[i]);
+      }
+    }
 	}
 }
